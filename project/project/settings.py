1861d48c41b5240bb6a309b720209676b53a30a4
Ahmed-Osama-Fawzy <ahmd.osama2611@mail.com>
2024-05-21 12:41:20 +0300

    FIles

--- project/project/settings.py
+++ project/project/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR , 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR , 'project/static')]
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "project/static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = '/media/'
